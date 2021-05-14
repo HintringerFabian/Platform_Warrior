c3dfb2bcff1bc971d8e2f3a709230ef1d5c83cad
Fabian Hintringer <fabianhintringer01@gmail.com>
2021-05-14 19:00:20 +0200

    Scripts: Cleanup code and comment everything

--- Platform_Warrior/Assets/Scipts/Camera/CameraFollow.cs
+++ Platform_Warrior/Assets/Scipts/Camera/CameraFollow.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     private const string PLAYER_TAG = "Player";
+
     private const float cameraOffsetY = 2f;
     private const float cameraOffsetX = 7f;
     private const float smoothSpeed = 5f;
 
-    private Transform playerPosition;
     private Vector3 tempPos;
-    Vector3 desiredPosition;
+    private Vector3 desiredPosition;
+    private Vector3 cameraOffsetVectorX;
+    private Vector3 playerPosition;
 
-    GameObject playerObject;
-    PlayerMovement playerMov;
+    private GameObject playerObject;
+    private PlayerMovement playerMov;
 
     // Start is called before the first frame update
     void Start() {
-        playerPosition = GameObject.FindWithTag(PLAYER_TAG).transform;
         playerObject = GameObject.FindWithTag(PLAYER_TAG);
+        cameraOffsetVectorX = new Vector3(cameraOffsetX, 0f, 0f);
     }
 
     // Update is called once per frame
@@ -28,23 +30,32 @@ public class CameraFollow : MonoBehaviour {
     }
 
     void setCameraPosition() {
-        playerMov = playerObject.GetComponent<PlayerMovement>();
-
+        // get position of Camera
         tempPos = transform.position;
 
-        // follow main character on Y axis
-        tempPos.y = playerPosition.position.y + cameraOffsetY;
+        // get position of Player
+        playerPosition = playerObject.transform.position;        
+        // get PlayerMovement variables from other script
+        playerMov = playerObject.GetComponent<PlayerMovement>();
 
-        
+        // camera switching left or right from player according to direction player is running
         if( playerMov.runningRight == true ) {
-            desiredPosition = playerPosition.position + new Vector3(cameraOffsetX, 0f, 0f);
+            desiredPosition = playerPosition + cameraOffsetVectorX;
         } else if (playerMov.runningLeft == true ) {
-            desiredPosition = playerPosition.position - new Vector3(cameraOffsetX, 0f, 0f);
+            desiredPosition = playerPosition - cameraOffsetVectorX;
         }
 
-        Vector3 smoothenedPosition = Vector3.Lerp(tempPos, desiredPosition, smoothSpeed * Time.deltaTime);
+        // smoothening camera
+        // Lerp is a smoothening function 
+        tempPos.x = Vector3.Lerp(tempPos, desiredPosition, smoothSpeed * Time.deltaTime).x;
 
-        tempPos.x = smoothenedPosition.x;
+        // follow main character on Y axis
+        tempPos.y = playerPosition.y + cameraOffsetY;
+
+        //
+        // Kathrin says she want to see the ground if when jumping
+        //
+        //
 
         // apply the changes onto the camera
         transform.position = tempPos;
