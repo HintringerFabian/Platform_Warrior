ca8336018b88729a44a12f1959114a3ebe6ad3c2
Fabian Hintringer <fabianhintringer01@gmail.com>
2021-05-14 18:55:09 +0200

    Scripts: Update and comments in script

--- Platform_Warrior/Assets/Scipts/Player/PlayerMovement.cs
+++ Platform_Warrior/Assets/Scipts/Player/PlayerMovement.cs
@@ -22,15 +22,11 @@ public class PlayerMovement : MonoBehaviour {
     private const string JUMP_ANIMATION = "Jump";
     private const string GROUND_TAG = "Ground";
 
-    private void Awake() {
+    // Start is called before the first frame update
+    void Start() {
         rigBod = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    // Start is called before the first frame update
-    void Start() {
-
     }
 
     // Update is called once per frame
@@ -39,25 +35,27 @@ public class PlayerMovement : MonoBehaviour {
         animateCharacter();
     }
 
+    // fixedUpdate will be called every 0.02 seconds
     void FixedUpdate() {
         playerJump();
     }
 
+    // get input for running
     void playerKeyboardInput() {
         moveX = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(moveX, 0f) * Time.deltaTime * moveSpeed;
     }
 
+    // get input for jumping
     void playerJump() {
         if( Input.GetButton("Jump") && jumping == false) {
-            Debug.Log("Player should jump");
             rigBod.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             jumping = true;
             animator.SetTrigger(JUMP_ANIMATION);
         }
-
     }
 
+    // animation for the movement
     private void animateCharacter() {
         if( moveX < 0 ) {
             // running left side
@@ -79,6 +77,9 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
+
+    // when landing back on the ground set the bool jumping to false
+    // then the character will be able to jump again
     private void OnCollisionEnter2D(Collision2D collision) {
         if( collision.gameObject.CompareTag(GROUND_TAG) ) {
             jumping = false;
