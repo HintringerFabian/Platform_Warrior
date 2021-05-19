d1012dddf62ca7d1cad8bf13f03ac7bd072067fb
Fabian Hintringer <fabianhintringer01@gmail.com>
2021-05-19 17:48:26 +0200

    Script: add script for plattform spawning

--- Platform_Warrior/Assets/Scipts/Background/PlattformSpawner.cs
+++ Platform_Warrior/Assets/Scipts/Background/PlattformSpawner.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlattformSpawner : MonoBehaviour
-{
+public class PlattformSpawner : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] plattform;
+
+    bool plattformSpawned;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        plattformSpawned = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D( Collision2D collision ) {
+        if ( !plattformSpawned ) {
+            int plattformNr = Random.Range( 0 , plattform.Length );
+
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            GameObject spawnedPlattform = Instantiate( plattform[plattformNr] );
+            SpriteRenderer spawnedSprite = spawnedPlattform.GetComponent<SpriteRenderer>();
+
+            int randomNegPos = ( Random.Range( 0 , 2 ) * 2 ) - 1;
+            float yOffset = System.Convert.ToSingle( Random.Range( 3 , 7 ) );
+            float xOffset = (System.Convert.ToSingle( Random.Range( 4 , 8 ) )
+                + ( sprite.bounds.size.x + spawnedSprite.bounds.size.x ) / 2 )
+                * randomNegPos;
+
+            spawnedPlattform.transform.position = transform.position + new Vector3( xOffset , yOffset );
+
+            plattformSpawned = true;
+        }
     }
 }
